3bb5fcd5ede6a4c0e4fba2737bdef5e7f587cf57
RaMadaSilva <raul01mateia@gmail.com>
2023-10-12 15:16:21 +0100

    creando a interface de repositorio base

---
+++ BrasilGeo.Domain/Repositories/IBaseRepository.cs
@@ -0,0 +1,13 @@
+﻿using BrasilGeo.Domain.Entities;
+
+namespace BrasilGeo.Domain.Repositories
+{
+    public interface IBaseRepository<TEntity> where TEntity : BaseEntity
+    {
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task<TEntity> GetByIdAsync (long id);
+        Task SaveAsync(TEntity entity);
+        Task UpdateAsync(TEntity entity);
+        Task DeleteAsync(long id);
+    }
+}

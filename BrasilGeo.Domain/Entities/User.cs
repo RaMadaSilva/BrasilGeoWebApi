cd54140840962dea6acbd0abc8e403dedd11046a
RaMadaSilva <raul01mateia@gmail.com>
2023-10-13 07:49:00 +0100

    Refatorando User

--- BrasilGeo.Domain/Entities/User.cs
+++ BrasilGeo.Domain/Entities/User.cs
@@ -5,14 +5,14 @@ namespace BrasilGeo.Domain.Entities
     public class User : BaseEntity, IEquatable<User>
     {
         private readonly List<Role> _roles = new();
-        public User(Email email, string password)
+        public User(Email email, Password password)
         {
             Email = email;
-            Password = password;
+            PasswordHash = password;
         }
 
         public Email Email { get; private set; }
-        public string Password { get; private set; }
+        public Password PasswordHash { get; private set; }
         IReadOnlyCollection<Role> Roles => _roles;
 
         public void AddRole(Role role) => _roles.Add(role);
---
+++ BrasilGeo.Domain/ValueObjects/Password.cs
@@ -0,0 +1,31 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace BrasilGeo.Domain.ValueObjects
+{
+    public class Password : ValueObject
+    {
+        public Password(string pass)
+        {
+            Pass = EncryptPassword(pass);
+        }
+
+        public string Pass { get; private set; }
+
+        public static implicit operator Password(string pass) =>new Password(pass);
+
+        public static implicit operator string(Password password) => password.ToString(); 
+
+        private string EncryptPassword(string password)
+        {
+            var md5 = MD5.Create();
+            var code = Encoding.ASCII.GetBytes(password);
+            var hash = md5.ComputeHash(code);
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < hash.Length; i++)
+                sb.Append(hash[i].ToString("X2"));
+            return sb.ToString();
+        }
+    }
+}

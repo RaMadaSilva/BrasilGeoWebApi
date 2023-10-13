d0fcac5d005aeee95a4623d305111b17ea09f7a5
RaMadaSilva <raul01mateia@gmail.com>
2023-10-13 16:09:53 +0100

    repostorios

--- BrasilGeo.Infra/Repositories/UniteOfWork.cs
+++ BrasilGeo.Infra/Repositories/UniteOfWork.cs
@@ -1,10 +1,5 @@
 ﻿using BrasilGeo.Domain.Repositories;
 using BrasilGeo.Infra.Context;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BrasilGeo.Infra.Repositories
 {

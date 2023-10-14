1310773c05c645b381b13e0d9cd84ca69fd45768
RaMadaSilva <raul01mateia@gmail.com>
2023-10-14 17:25:12 +0100

    refarorando o LocationIBGEConfiguration

--- BrasilGeo.Infra/Configurations/LocationIBGEConfiguration.cs
+++ BrasilGeo.Infra/Configurations/LocationIBGEConfiguration.cs
@@ -9,10 +9,28 @@ namespace BrasilGeo.Infra.Configurations
         public void Configure(EntityTypeBuilder<LocationIBGE> builder)
         {
             builder.ToTable(nameof(LocationIBGE));
+
             builder.HasKey(property => property.Id);
-            builder.Property(property => property.Id).IsRequired();
-            builder.Property(property => property.City).IsRequired();
-            builder.Property(property => property.State).IsRequired();
+            builder.Property(property => property.Id)
+                .IsRequired()
+                .HasColumnName("Id")
+                .HasColumnType("BIGINT");
+            builder.Property(property => property.City)
+                .IsRequired()
+                .HasColumnName("City")
+                .HasColumnType("NVARCHAR")
+                .HasMaxLength(200);
+            builder.OwnsOne(x=>x.State)
+                .Property(property => property.Uf)
+                .IsRequired()
+                .HasColumnName("State")
+                .HasColumnType("NVARCHAR")
+                .HasMaxLength(5);
+
+            //Indexar
+            builder.HasIndex(x => x.State, "IX_Local_State");
+            builder.HasIndex(x => x.City, "IX_Local_City")
+                .IsUnique();
         }
     }
 }

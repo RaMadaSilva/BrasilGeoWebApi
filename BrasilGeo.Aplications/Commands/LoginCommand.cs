ee39e33d8eaeb69f230e44b3a98a5b177339c803
RaMadaSilva <raul01mateia@gmail.com>
2023-10-14 19:05:47 +0100

    login command

---
+++ BrasilGeo.Aplications/Commands/LoginCommand.cs
@@ -0,0 +1,20 @@
+﻿using BrasilGeo.Domain.Commands.Interfaces;
+using Flunt.Notifications;
+using Flunt.Validations;
+
+namespace BrasilGeo.Aplications.Commands
+{
+    public class LoginCommand : Notifiable<Notification>, ICommand
+    {
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+
+        public void Valid()
+        {
+            AddNotifications(new Contract<LoginCommand>()
+                .Requires()
+                .IsEmail(Email, "E-mail", "Campo de Email")
+                .IsGreaterThan(Password, 8, "Passwors", "Deve conter mais de 8 caracteres"));
+        }
+    }
+}
---
+++ BrasilGeo.Aplications/Dtos/LoginDto.cs
@@ -0,0 +1,8 @@
+﻿namespace BrasilGeo.Aplications.Dtos
+{
+    public class LoginDto
+    {
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty; 
+    }
+}

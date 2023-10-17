e07199d9166cb41f85312638696724a1fb54eb73
Leonardo Oliveira <leozartino@gmail.com>
2023-10-17 18:14:32 +0200

    [#2]feat: create, upate and delete for Location

---
+++ BrasilGeo.Api/Controllers/LocationIBGEController.cs
@@ -0,0 +1,61 @@
+﻿using BrasilGeo.Aplications.Commands;
+using BrasilGeo.Aplications.Handlers.LocationIBGEHandler;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BrasilGeo.Api.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class LocationIBGEController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateLocation([FromBody] CreateLocationIBGECommand command, 
+            [FromServices] CreateLocationIBGEHandler handler)
+        {
+            var result = await handler.HandleAsync(command);
+
+            if (!result.Sucess)
+                return BadRequest(result);
+
+            return Ok(result);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateLocation([FromBody] UpdateLocationIBGECommand command, 
+            [FromServices] UpdateLocationIBGEHandler handler)
+        {
+            try
+            {
+                var result = await handler.HandleAsync(command);
+
+                if (!result.Sucess)
+                    return BadRequest(result);
+
+                return Ok(result);
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult>  DeleteLocation([FromBody] DeleteLocationIBGECommand command,
+            [FromServices] DeleteLocationIBGEHandler handler)
+        {
+            var result = await handler.HandleAsync(command);
+
+            if (!result.Sucess)
+                return BadRequest(result);
+
+            return Ok(result);
+        }
+    }
+}

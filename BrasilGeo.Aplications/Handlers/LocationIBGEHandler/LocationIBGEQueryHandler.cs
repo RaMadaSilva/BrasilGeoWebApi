c8ee84520c772b41e1c6f729574e81080f176d60
erezendeosg <erezende@osg.com.br>
2023-10-18 13:31:46 -0300

    adiciona LocationIBGEQuery e inicia implementação LocationIBGEHandler

---
+++ BrasilGeo.Aplications/Handlers/LocationIBGEHandler/LocationIBGEQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BrasilGeo.Aplications.Dtos;
+using BrasilGeo.Aplications.Queries;
+using BrasilGeo.Domain.Handlers;
+
+namespace BrasilGeo.Aplications.Handlers.LocationIBGEHandler
+{
+    public class LocationIBGEQueryHandler : IQueryHandler<LocationIBGEQuery,LocationIBGEDto>
+    {
+        public Task<LocationIBGEDto> HandleAsync(LocationIBGEQuery query)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

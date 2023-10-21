36f93bb9bf02c0d8538631fe0da585d5bad1e474
RaMadaSilva <raul01mateia@gmail.com>
2023-10-21 21:47:11 +0100

    parte da documentacao

---
+++ BrasilGeo.Test/DomainTest/ValueObjectTest/EmailTest.cs
@@ -0,0 +1,27 @@
+﻿namespace BrasilGeo.Test.DomainTest.ValueObjectTest
+{
+    public class EmailTest
+    {
+        [Fact]
+        public void ValueObject_ShouldSucess_WhenUserValid()
+        {
+            //arrage
+
+            //Act
+
+            //Assert
+        }
+        [Fact]
+        public void ValueObject_ShouldError_WhenEmailNullOrEmpty()
+        {
+            //Arrage
+
+            //Act
+
+            //assert
+
+        }
+
+     
+    }
+}
